5e9820818c17bbbe22154b6abdb8a0281b1d3142
Vitorhr10 <vitorrodrigueshr10@gmail.com>
2020-11-02 14:44:16 -0300

    App name changed

--- app/src/main/java/com/example/meusamigos2/MainActivity.java
+++ app/src/main/java/com/example/meusamigos2/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().hide();
+        //getSupportActionBar().hide();
 
         if (lista_deletados) {
             configurarRecyclerView(0);
